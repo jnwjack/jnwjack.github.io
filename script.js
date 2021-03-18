2c1a60488f24423baf0206d51cf9041a81461fcf
Jack Wohl <jnw900@gmail.com>
2021-03-18 05:48:38 -0400

    first complete draft

--- script.js
+++ script.js
@@ -9,4 +9,28 @@ const iO = new IntersectionObserver(entries => entries.forEach(entry => {
 });
 
 panels = Array.from(document.querySelectorAll('article'))
-window.addEventListener('DOMContentLoaded', () => panels.map(a => iO.observe(a)));``
+window.addEventListener('DOMContentLoaded', () => panels.map(a => iO.observe(a)));
+
+function insertBookingEngine() {
+  // Add script to insert widget
+  let script = document.createElement('script');
+  script.id = 'booking-engine-script';
+  script.src = 'https://umi-booking-engine.herokuapp.com/embed.js';
+  document.body.appendChild(script);
+
+  // Change button callback to function that removes widget
+  let cardLink = document.querySelector('#bookingengine .card-link');
+  cardLink.onclick = removeBookingEngine;
+  cardLink.textContent = "Remove Widget";
+}
+
+function removeBookingEngine() {
+  // Remove script and widget
+  document.getElementById('booking-engine-script').remove();
+  document.getElementById('booking-widget').remove();
+
+  // Change button callback to function that inserts widget
+  let cardLink = document.querySelector('#bookingengine .card-link');
+  cardLink.onclick = insertBookingEngine;
+  cardLink.textContent = "View it Here";
+}
